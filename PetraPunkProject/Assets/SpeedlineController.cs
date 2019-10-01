e5685affa583f7ae72432ca1c44aff3ba59c824c
Martin Krøll <martino1215@hotmail.com>
2019-10-01 12:30:21 +0200

    SpeedLines Work

--- PetraPunkProject/Assets/SpeedlineController.cs
+++ PetraPunkProject/Assets/SpeedlineController.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class SpeedlineController : MonoBehaviour
 {
+    public PlayerController pc;
+    public GameObject speedLines;
+    private bool isActive;
     // Start is called before the first frame update
     void Start()
     {
-        
+        speedLines.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(pc.OnSlope && !isActive)
+        {
+            speedLines.SetActive(true);
+            isActive = true;
+        }
+
+        if (!pc.OnSlope && isActive)
+        {
+            speedLines.SetActive(false);
+            isActive = false;
+        }
+
+
     }
 }
