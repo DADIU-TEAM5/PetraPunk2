c6b1a8c8ab359b57f4b4e3ec68e2df1fa26beda0
Magnus <magnusmhp@gmail.com>
2019-10-07 11:13:40 +0200

    editor script

---
+++ PetraPunkProject/Assets/Editor/PlayerControllerEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PlayerController))]
+public class PlayerControllerEditor : Editor
+{
+
+    PlayerController playerController;
+    // Start is called before the first frame update
+
+
+    private void OnEnable()
+    {
+
+        playerController = (PlayerController) target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+
+        base.OnInspectorGUI();
+    }
+
+}
